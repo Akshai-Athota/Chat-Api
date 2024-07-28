018747996ec85e4776708354dd5689f8dc86a43e
Akshai-Athota <akshaiathotaspring001@gmail.com>
2024-07-28 09:59:39 +0530

    initial repo set up

---
+++ src/main/java/com/akshai/web/sockets/chat/Api/WebSocketsChatApiApplication.java
@@ -0,0 +1,13 @@
+package com.akshai.web.sockets.chat.Api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebSocketsChatApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebSocketsChatApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/akshai/web/sockets/chat/Api/WebSocketsChatApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akshai.web.sockets.chat.Api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebSocketsChatApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
